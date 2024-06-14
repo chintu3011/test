5c685dd52519c9640d8f08d47c82c690e2a6a218
Chintan patel <chintan.amrisystems@gmail.com>
2024-06-14 15:48:34 +0530

    Intial commite

---
+++ app/src/main/java/com/example/secondproject/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.secondproject
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tv = findViewById<TextView>(R.id.tvOpen)
+        tv.setOnClickListener {
+            try {
+                val myIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://logicflashtest.000webhostapp.com/book"))
+                startActivity(myIntent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(
+                    this, "No application can handle this request."
+                            + " Please install a webbrowser", Toast.LENGTH_LONG
+                ).show()
+                e.printStackTrace()
+            }
+        }
+    }
+}
